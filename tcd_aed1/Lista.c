847db09c01e954e59f5a385a7a4305d52d92a0b1
supercasao <100032236+supercasao@users.noreply.github.com>
2023-06-03 18:26:55 -0300

    Update Lista.c #casão

--- tcd_aed1/Lista.c
+++ tcd_aed1/Lista.c
@@ -1,100 +1,168 @@
 #include <stddef.h>
 
-typedef struct polinomio {
-    int id;
-    int *coeficientes;
-    int *potencias;
-} Poli;
-/*
-struct do tipo polinomio, com tres campos
-id - identificador do polinomio
-coeficientes - ponteiro que aponta para um vetor alocado dinamicamente que armazena os coeficientes
-potencias - ponteiro que aponta para um vetor alocado dinamicamente que armazena as potencias
-*/
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct lista {
-    No *inicio;
-    int tam;
-} Lista;
-/*
-lista encadeada
-inicio - ponteiro do tipo No que aponta para a primeira posicao
-tam - armazena o numero de polinomios na lista
-*/
-
-typedef struct no {
-    Poli valor;
-    struct no *prox;
-} No;
-/*
-
-*/
-
-Lista* criar () {
-    Lista *l = (Lista*) malloc (sizeof (Lista));
-    if (l == NULL) return 2;
-    l -> inicio = NULL;
-    return l;    
+Lista* criar_lista() {
+    Lista* lista = (Lista*)malloc(sizeof(Lista));
+    lista->inicio = NULL;
+    lista->fim = NULL;
+    lista->tamanho = 0;
+    return lista;
 }
-/*
-funcao que cria uma lista e retorna um ponteiro 
-*/
 
-int listavazia (Lista*l) {
-    if (l == NULL) return 2;
-    if (l -> inicio == NULL) return 0;
-    return 1;
+void destruir_lista(Lista* lista) {
+    No* no_atual = lista->inicio;
+    while (no_atual != NULL) {
+        No* proximo = no_atual->proximo;
+        free(no_atual);
+        no_atual = proximo;
+    }
+    free(lista);
 }
-/*
-funcao que verifica se a lista esta vazia, retorna 0 caso esteja e 1 caso nao esteja
-*/
 
-int inserirpolinomio (Lista *l, Poli x) {
-    
+void inserir_inicio(Lista* lista, int valor) {
+    No* novo_no = (No*)malloc(sizeof(No));
+    novo_no->valor = valor;
+    novo_no->proximo = lista->inicio;
+    lista->inicio = novo_no;
+    if (lista->fim == NULL) {
+        lista->fim = novo_no;
+    }
+    lista->tamanho++;
 }
-/*
-funcao que insere um novo polinomio na lista
-*/
 
-int inseretermo (Lista *l, int idx, int coef, int pot) {
-
+void inserir_fim(Lista* lista, int valor) {
+    No* novo_no = (No*)malloc(sizeof(No));
+    novo_no->valor = valor;
+    novo_no->proximo = NULL;
+    if (lista->fim == NULL) {
+        lista->inicio = novo_no;
+    } else {
+        lista->fim->proximo = novo_no;
+    }
+    lista->fim = novo_no;
+    lista->tamanho++;
 }
-/*
-funcao que insere um novo termo em um polinomio da lista 
-a funcao recebe uma lista de polinomios, um identificador para qual polinomio deve ser alterado na lista, e o coeficiente e potencia do termo adicionado
-*/
 
-void mostrar (Lista *l, int idx) {
+void inserir_posicao(Lista* lista, int valor, int posicao) {
+    if (posicao < 0 || posicao > lista->tamanho) {
+        printf("Posição inválida.\n");
+        return;
+    }
+    if (posicao == 0) {
+        inserir_inicio(lista, valor);
+    } else if (posicao == lista->tamanho) {
+        inserir_fim(lista, valor);
+    } else {
+        No* novo_no = (No*)malloc(sizeof(No));
+        novo_no->valor = valor;
 
+        No* no_anterior = lista->inicio;
+        int i;
+        for (i = 1; i < posicao; i++) {
+            no_anterior = no_anterior->proximo;
+        }
+
+        No* no_proximo = no_anterior->proximo;
+        no_anterior->proximo = novo_no;
+        novo_no->proximo = no_proximo;
+
+        lista->tamanho++;
+    }
 }
-/*
-funcao que mostra ao usuario o polinomio da lista l e de identificador idx com a formatacao especificada
-*/
 
-int removerk (Lista *l, int idx, int k) {
+void remover_inicio(Lista* lista) {
+    if (lista->inicio == NULL) {
+        printf("A lista está vazia.\n");
+        return;
+    }
 
+    No* no_removido = lista->inicio;
+    lista->inicio = lista->inicio->proximo;
+    free(no_removido);
+    lista->tamanho--;
+
+    if (lista->inicio == NULL) {
+        lista->fim = NULL;
+    }
 }
-/*
-funcao que recebe uma lista, um identificador idx e elimina o termo associado a k-esima potencia, fazendo os ajustes necessarios para o polinomio continuar valido
-*/
 
-int reinicializar (Lista *l, int idx) {
+void remover_fim(Lista* lista) {
+    if (lista->fim == NULL) {
+        printf("A lista está vazia.\n");
+        return;
+    }
 
+    if (lista->inicio == lista->fim) {
+        free(lista->inicio);
+        lista->inicio = NULL;
+        lista->fim = NULL;
+    } else {
+        No* no_anterior = lista->inicio;
+        while (no_anterior->proximo != lista->fim) {
+            no_anterior = no_anterior->proximo;
+        }
+
+        free(lista->fim);
+        no_anterior->proximo = NULL;
+        lista->fim = no_anterior;
+    }
+
+    lista->tamanho--;
 }
-/*
-funcao que recebe uma lista e um identificador idx e reinicializa o polinomio indicado para a forma 0x^0
-*/
 
-int soma (Lista *l, int idx, Lista *J, int idy) {
+void remover_posicao(Lista* lista, int posicao) {
+    if (posicao < 0 || posicao >= lista->tamanho) {
+        printf("Posição inválida.\n");
+        return;
+    }
 
+    if (posicao == 0) {
+        remover_inicio(lista);
+    } else if (posicao == lista->tamanho - 1) {
+        remover_fim(lista);
+    } else {
+        No* no_anterior = lista->inicio;
+        int i;
+        for (i = 1; i < posicao; i++) {
+            no_anterior = no_anterior->proximo;
+        }
+
+        No* no_removido = no_anterior->proximo;
+        No* no_proximo = no_removido->proximo;
+        no_anterior->proximo = no_proximo;
+        free(no_removido);
+
+        lista->tamanho--;
+    }
 }
-/*
-funcao que soma dois polinomios, recebe a lista e o identificador de cada polinomio
-*/
 
-int valorpx (Lista *l, int idx, int num) {
+int obter_elemento(Lista* lista, int posicao) {
+    if (posicao < 0 || posicao >= lista->tamanho) {
+        printf("Posição inválida.\n");
+        return -1;
+    }
 
+    No* no_atual = lista->inicio;
+    int i;
+    for (i = 0; i < posicao; i++) {
+        no_atual = no_atual->proximo;
+    }
+
+    return no_atual->valor;
 }
-/*
-funcao que calcula o valor de p(x) para um valor x solicitado
-*/
+
+int tamanho_lista(Lista* lista) {
+    return lista->tamanho;
+}
+
+void exibir_lista(Lista* lista) {
+    No* no_atual = lista->inicio;
+    while (no_atual != NULL) {
+        printf("%d ", no_atual->valor);
+        no_atual = no_atual->proximo;
+    }
+    printf("\n");
+}
