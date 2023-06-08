7d407070c9e5efbc673416a86bb966d645bea7a6
VitorHOS <114543236+VitorHOS@users.noreply.github.com>
2023-06-07 22:57:04 -0300

    Update polinomio.h

--- tcd_aed1/polinomio.h
+++ tcd_aed1/polinomio.h
@@ -1,1 +1,13 @@
+#ifndef POLINOMIO_H_INCLUDED
+#define POLINOMIO_H_INCLUDED
 
+Polinomio* inicializarPolinomio();
+void exibirPolinomio(Polinomio* polinomio);
+void inserirTermo(Polinomio* polinomio, int coeficiente, int expoente);
+void eliminarTermo(Polinomio* polinomio, int expoente);
+void reinicializarPolinomio(Polinomio* polinomio);
+Polinomio* somarPolinomios(Polinomio* polinomio1, Polinomio* polinomio2);
+int calcular_valorPolinomio(Polinomio* polinomios,  float x);
+void destruirPolinomio(Polinomio* polinomio);
+
+#endif // POLINOMIO_H_INCLUDED
