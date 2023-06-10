22283cb21f03ebc2112508b9ebccc0e63ad94a22
supercasao <100032236+supercasao@users.noreply.github.com>
2023-06-10 15:45:39 -0300

    Update polinomio.h somar

--- tcd_aed1/polinomio.h
+++ tcd_aed1/polinomio.h
@@ -6,7 +6,7 @@ void exibirPolinomio(Polinomio* polinomio);
 void inserirTermo(Polinomio* polinomio, int coeficiente, int expoente);
 void eliminarTermo(Polinomio* polinomio, int expoente);
 void reinicializarPolinomio(Polinomio* polinomio);
-Polinomio* somarPolinomios(Polinomio* polinomio1, Polinomio* polinomio2);
+Polinomio* somarPolinomios(int id1, int id2);
 int calcular_valorPolinomio(Polinomio* polinomios,  float x);
 void destruirPolinomio(Polinomio* polinomio);
 
